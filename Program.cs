7b912603cc8a112ec18459292eb056d5eef12be0
vivek <vivek@athenaeum.in>
2021-06-02 11:41:50 +0530

    done

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CSharp.LINQ.Study
 {
@@ -6,32 +7,24 @@ namespace CSharp.LINQ.Study
     {
         static void Main(string[] args)
         {
-            //  creating a string array
-            string[] names = new string[5]; //  traditional declaration
-
-            //  all elements are initialized to their default values (numeric types -> 0, reference types -> null, bool -> false)
-            double[] averages = new double[10];
-
-            //  intializer syntax (verbose)
-            string[] friends = new string[] { "Sujit", "Kumar", "Puja", "Raman" };
-
-            //  initializer syntax (modern)
-            string[] books = { "C# Programming Language", "Pro ASP.NET Core" };
-
-            //  var can be used, but on the declaration we must have new type[]
-            var scores = new int[] { 125, 478, 514, 714 };
-
-            //  All arrays are reference types in C#. They inherit from Array base class
-            Console.WriteLine(scores is Array);
-
-            //  since Array implements IEnumerable, we can use arrays in foreach
-            foreach (var item in scores)
+            IEnumerable<int> coll = GetNumbers();
+            
+            foreach (var n in coll)
             {
-                Console.WriteLine(item);
+                Console.WriteLine(n);
             }
+        }
 
-            //  accessing the members of Array base class
-            Console.WriteLine(books.Length);
+        //  https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/yield
+        static IEnumerable<int> GetNumbers()
+        {
+            Console.WriteLine("GetNumbers is called");
+            Random random = new Random();
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("inside the generator");
+                yield return random.Next();
+            }
         }
     }
 }
