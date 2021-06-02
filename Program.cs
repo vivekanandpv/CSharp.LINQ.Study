5a1de8a1b4f66a3475503dfb442d4855d133f9a4
vivek <vivek@athenaeum.in>
2021-06-02 13:48:52 +0530

    done

--- Program.cs
+++ Program.cs
@@ -8,36 +8,11 @@ namespace CSharp.LINQ.Study
     {
         static void Main(string[] args)
         {
-            var groupResult1 = GetSequence()
-                .Where(n => n % 2 == 0)
-                .GroupBy(k => "Even");
-
-            foreach (var item in groupResult1)
-            {
-                Console.WriteLine($"Key: {item.Key}");
-                foreach (var e in item)
-                {
-                    Console.WriteLine($"Value: {e}");
-                }
-            }
-
-            Console.WriteLine("--------------------------------------");
-
-            var groupResult2 = GetSequence()
-                .Where(n => n % 2 != 0)
-                .GroupBy(k => "Squares of odd numbers", k => k * k);
-
-            foreach (var item in groupResult2)
-            {
-                Console.WriteLine($"Key: {item.Key}");
-                foreach (var e in item)
-                {
-                    Console.WriteLine($"Value: {e}");
-                }
-            }
+            //  concatenates two sequences (second starts after first finishes)
+           int[] combined = GetSequence1().Concat(GetSequence2()).ToArray();
         }
 
-        static IEnumerable<int> GetSequence()
+        static IEnumerable<int> GetSequence1()
         {
             int i = 1;
 
@@ -45,6 +20,17 @@ namespace CSharp.LINQ.Study
             {
                 yield return i;
                 ++i;
+            }
+        }
+
+        static IEnumerable<int> GetSequence2()
+        {
+            int i = 100;
+
+            while (i >= 90)
+            {
+                yield return i;
+                --i;
             }
         }
     }
