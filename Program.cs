fa4fa3f13f16ae87f87d8b6f0e2b38403e974465
vivek <vivek@athenaeum.in>
2021-06-02 14:45:33 +0530

    done

--- Program.cs
+++ Program.cs
@@ -9,7 +9,9 @@ namespace CSharp.LINQ.Study
         static void Main(string[] args)
         {
             //  concatenates two sequences (second starts after first finishes)
-           int[] combined = GetSequence1().Concat(GetSequence2()).ToArray();
+           int[] combined = GetSequence1().Concat(GetSequence2()).ToArray();    //  preserves duplicates
+           int[] combined2 = GetSequence1().Union(GetSequence2()).ToArray();    //  no duplicates
+           int[] combined3 = GetSequence2().Except(GetSequence1()).ToArray();   //  set difference
         }
 
         static IEnumerable<int> GetSequence1()
@@ -25,9 +27,9 @@ namespace CSharp.LINQ.Study
 
         static IEnumerable<int> GetSequence2()
         {
-            int i = 100;
+            int i = 16;
 
-            while (i >= 90)
+            while (i >= 0)
             {
                 yield return i;
                 --i;
