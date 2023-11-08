e9fda66016963bc29c429dd61f94d9119cf75a25
Vivekanand P V <vivekanandpv@gmail.com>
2023-11-08 15:47:04 +0530

    done

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CSharp.LINQ.Study
 {
@@ -6,7 +8,41 @@ namespace CSharp.LINQ.Study
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Person> persons = new List<Person>
+            {
+                new Person {FullName="Rajan Mishra", PersonId=2},
+                new Person {FullName="Ramesh Kumar", PersonId=3},
+                new Person {FullName="Manas Chandra", PersonId=5},
+            };
+
+            List<Car> cars = new List<Car>
+            {
+                new Car {RegistrationNumber=4478, PersonId=2},
+                new Car {RegistrationNumber=5689, PersonId=3},
+                new Car {RegistrationNumber=7750, PersonId=5},
+            };
+
+            List<string> joined =
+                 persons
+                 .Join(
+                     cars,
+                     p => p.PersonId,
+                     c => c.PersonId,
+                     (p, c) => $"Person: {p.FullName} Registration Number: {c.RegistrationNumber}"
+                     )
+                 .ToList();
         }
+    }
+
+    class Person
+    {
+        public int PersonId { get; set; }
+        public string FullName { get; set; }
+    }
+
+    class Car
+    {
+        public int RegistrationNumber { get; set; }
+        public int PersonId { get; set; }
     }
 }
